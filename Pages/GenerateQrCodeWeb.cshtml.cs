8d8d790abb7dca10e70c3454a03da5dc49880ef6
Jeú <jeufrancosilva@gmail.com>
2023-04-05 12:27:42 -0300

    Add files via upload

---
+++ Pages/GenerateQrCodeWeb.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace render_qrcode_web.Pages;
+
+public class GenerateQrCodeWebModel : PageModel
+{
+    private readonly ILogger<GenerateQrCodeWebModel> _logger;
+
+    public GenerateQrCodeWebModel(ILogger<GenerateQrCodeWebModel> logger)
+    {
+        _logger = logger;
+    }
+
+   [Required(ErrorMessage="Favor informar o conteúdo do QR Code!")]
+    public string ContentQrCode { get; set; } = null!;
+
+    public void OnGet()
+    {
+    }
+}
+
---
+++ Pages/RenderQrCodeWeb.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace render_qrcode_web.Pages;
+
+public class RenderQrCodeWebModel : PageModel
+{
+    private readonly ILogger<RenderQrCodeWebModel> _logger;
+
+    public RenderQrCodeWebModel(ILogger<RenderQrCodeWebModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public string imageSource = "";
+
+    public void OnPost(string content)
+    {
+        imageSource = QrCodeServices.BitpmapToImageSource(Request.Form["ContentQrCode"]);
+    }
+}
+
